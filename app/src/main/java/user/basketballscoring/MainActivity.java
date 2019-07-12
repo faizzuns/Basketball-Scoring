8e45ce5e329027015459a15765e0197a989cbb30
Ahmad Faiz Sahupala <13516065@std.stei.itb.ac.id>
2019-07-12 18:27:31 +0700

    milestone-2

--- app/src/main/java/user/basketballscoring/MainActivity.java
+++ app/src/main/java/user/basketballscoring/MainActivity.java
@@ -2,12 +2,46 @@ package user.basketballscoring;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    public final String TAG = "CheckActivityLifecycle";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate called!");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart called!");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume called!");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause called!");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop called!");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy called!");
     }
 }
